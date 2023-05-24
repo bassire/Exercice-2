527325282abf608621758502419b163defb30aa3
bassire <85232639+bassire@users.noreply.github.com>
2023-05-24 09:35:58 +0200

    Add files via upload

---
+++ exo1.py
@@ -0,0 +1,21 @@
+age = 80
+poids = 80.0
+initial = 'A'
+distance = 150000
+pi = 3.14159
+nombre = 100
+compte = 1000
+nom = "Davy Jones"
+population = 7896541230
+code = 12345
+
+print("Age:", age)
+print("Poids: %.2f" % poids)
+print("Initial:", initial)
+print("Distance:", distance)
+print("Pi:", pi)
+print("Nombre:", nombre)
+print("Compte:", compte)
+print("Nom:", nom)
+print("Population:", population)
+print("Code:", code)
